75196fe373d8de070f8b580951b48b6d20915e3e
Your Name <you@example.com>
2023-10-05 12:16:46 -0700

    function in cubit

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,7 +1,19 @@
 import 'package:basket/cubit/counter_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CounterCubit extends Cubit <CounterState>{
+class CounterCubit extends Cubit<CounterState> {
   CounterCubit(super.initialState);
 
+  int numA = 0;
+
+  int numB = 0;
+  void TeamIncrement({required String team, required int buttonNumber}) {
+    if (team == 'A') {
+      numA += buttonNumber;
+      emit(CounterAIncremantState());
+    } else {
+      numB += buttonNumber;
+      emit(CounterBIncremantState());
+    }
+  }
 }
